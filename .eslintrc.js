d0ede5985489d44b8ce4501ad43ff3d5ebdf5d61
Antony O'Neill <antony@ant-web.co.uk>
2020-06-05 08:31:25 +0100

    configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "googleappsscript/googleappsscript": true,
-        "es6": true
-    },
-    "plugins": [
-        "googleappsscript"
-    ],
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    'googleappsscript/googleappsscript': true,
+    es6: true,
+  },
+  plugins: ['googleappsscript'],
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    quotes: ['error', 'single'],
+  },
 };
